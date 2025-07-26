8135a6206dc57581bd7e700b4077e6e6fa013dac
phu ly <phu.lyg@gmail.com>
2025-07-25 22:40:18 -0500

    update to ebay standard

--- api/ebay/account-deletion.js
+++ api/ebay/account-deletion.js
@@ -1,10 +1,22 @@
-export default async function handler(req, res) {
-  const token = "TOKEN_FOR_DNCLTECHZONE_EBAY_PLATFORM_2025";
+import crypto from 'crypto';
 
-  if (req.method === "POST") {
-    res.setHeader("Content-Type", "text/plain");
-    res.status(200).send(token);
-  } else {
-    res.status(405).send("Method Not Allowed");
+const TOKEN = "TOKEN_FOR_DNCLTECHZONE_EBAY_PLATFORM_2025";
+const ENDPOINT = "https://your-deployed-url.vercel.app/ebay/account-deletion";
+
+export default function handler(req, res) {
+  if (req.method === 'GET' && req.query.challenge_code) {
+    const challenge = req.query.challenge_code;
+    const hash = crypto.createHash('sha256');
+    hash.update(challenge + TOKEN + ENDPOINT);
+    const challengeResponse = hash.digest('hex');
+    res.setHeader('Content-Type', 'application/json');
+    return res.status(200).json({ challengeResponse });
   }
+
+  if (req.method === 'POST') {
+    res.setHeader('Content-Type', 'text/plain');
+    return res.status(200).send(TOKEN);
+  }
+
+  res.status(405).send('Method Not Allowed');
 }
